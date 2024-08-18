e3ddaab436a91e9ddf4fef5791014c3b2e513b27
FIRST_NAME LAST_NAMErameshgit <adhikariramesh0039@gmail.com>
2024-08-17 20:38:26 -0500

    my second commit

--- src/assesment/multiplicationtable.java
+++ src/assesment/multiplicationtable.java
@@ -4,7 +4,7 @@ public class multiplicationtable {
 
 	public static void main(String[] args) {
 		int num=13;
-		for(int i=1;i<=20;i++)
+		for(int i=1;i<=10;i++)
 			
 		{System.out.println(num +"*"+i+"="+(num*i));
 		
